5b9871b020df2cba51e6e0cdca1b32d08b790efd
awei1127 <awei1127@hotmail.com>
2024-09-27 19:52:24 +0800

    修正動畫未出現的問題

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useCallback } from 'react';
+import './App.css';
 import { X, CheckCircle, Settings, Info } from 'lucide-react';
 
 const LevelUpAnimation = ({ onComplete }) => {
